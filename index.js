d62458140f4c265629093453f649b45f8ab24b7b
空巷一人 <1530688385@qq.com>
2022-04-12 15:52:00 +0800

    1.0.2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import ora from 'ora';
 import chalk from 'chalk';
 import fs from 'fs-extra';
 import templatePath from './template.js';
-program.version('1.0.1');
+program.version('1.0.2');
 
 program
   .command('create <app-name>')
